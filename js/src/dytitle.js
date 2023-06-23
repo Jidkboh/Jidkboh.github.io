5843c4cff20a7d8e8b326d210fc1dd895b3e5b17
Jidkboh <1254882896@qq.com>
2023-06-23 23:03:25 +0800

    Site updated: 2023-06-23 23:03:24

---
+++ js/src/dytitle.js
@@ -0,0 +1,16 @@
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="shortcut icon"]').attr('href', "/ic.png");
+        document.title = 'w(ﾟДﾟ)w 出BUG啦！！！！';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="shortcut icon"]').attr('href', "/w.png");
+        document.title = '♪(^∇^*)又好了. . . ' + OriginTitile;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
